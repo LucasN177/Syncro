83a5a74b1fe554b93e13a4fe5d559504f5c4be46
LucasN177 <lucas.nagelsmann@gmail.com>
2026-04-20 21:54:11 +0200

    Added packages

--- Program.cs
+++ Program.cs
@@ -1,11 +1,30 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using Supabase;
 using Syncro;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddMudServices();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var url = builder.Configuration["Supabase:Url"];
+var key = builder.Configuration["Supabase:AnonKey"];
+
+var options = new SupabaseOptions
+{
+    AutoRefreshToken = true,
+    AutoConnectRealtime = true
+};
+
+builder.Services.AddSingleton<Client>(provider =>
+    new Client(
+        url!, key, options
+    )
+);
 
 await builder.Build().RunAsync();
